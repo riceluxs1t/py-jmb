17c0fab02a1cf6f555098d6eb590ee6f57475a07
Seung Hun Jang <sj43@rice.edu>
2018-12-18 22:25:07 +0900

    BOGGLE - DP solution
    Need feedback on how to follow C++
    convention of using global cache (to keep function parameter as similar as possible to C++ function parameters)

--- ch6/boggle.py
+++ ch6/boggle.py
@@ -1,45 +1,59 @@
+from collections import defaultdict
+
 SIZE_OF_BOARD = 5
-board = []
+MAX_NUM_OF_ALPHABET_PER_WORD = 10
 dx = [-1, -1, -1, 1, 1, 1, 0, 0]
 dy = [-1, 0, 1, -1, 0, 1, -1, 1]
 
-
-def in_range(y, x):
+def in_range(y, x): 
     return x >= 0 and y >= 0 and x <= 4 and y <= 4
 
 
-def has_word(y, x, word):
+def has_word(y, x, word, cur_index, cache):
     if not in_range(y, x):
         return False
-    if board[y][x] != word[0]:
+    cache[y][x][cur_index] = 1
+    if board[y][x] != word[cur_index]:
         return False
-    if len(word) == 1:
+    if cur_index == len(word)-1:
         return True
     for direction in xrange(8):
         nextY = y + dy[direction]
         nextX = x + dx[direction]
-        if has_word(nextY, nextX, word[1:]):
+        if not in_range(nextY, nextX):
+            continue
+        if cache[nextY][nextX][cur_index+1]:
+            continue
+        if has_word(nextY, nextX, word, cur_index+1, cache):
             return True
     return False
 
 
-C = input()
-for _ in xrange(C):
-    for __ in xrange(SIZE_OF_BOARD):
+def initialize_cache():
+    cache = {} # dynamic programming - memoization
+    for y in xrange(SIZE_OF_BOARD): # initializing dp cache
+        cache[y] = defaultdict(int)
+        for x in xrange(SIZE_OF_BOARD):
+            cache[y][x] = defaultdict(int)
+    return cache
+
+
+for _ in xrange(int(raw_input())):
+    board = [] # board has to be emptied at the beginning of every test case
+    for _ in xrange(SIZE_OF_BOARD):
         board.append(raw_input().strip())
-    N = input()
-    for __ in range(N):
+    for _ in xrange(int(raw_input())):
+        cache = initialize_cache() # cache has to be emptied for every word.
         word_to_find = raw_input().strip()
         already_found = False
         for y in xrange(SIZE_OF_BOARD):
             for x in xrange(SIZE_OF_BOARD):
-                if has_word(y, x, word_to_find):
+                if has_word(y, x, word_to_find, 0, cache):
                     print word_to_find, 'YES'
                     already_found = True
                     break
-            else:
-                continue
-            break
+            if already_found:
+                break
         if not already_found:
             print word_to_find, 'NO'
                 
