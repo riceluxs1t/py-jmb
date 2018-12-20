2ec06e32e0667ae299322d0f3d00ec22f89f3c94
Seung Hun Jang <sj43@rice.edu>
2018-12-20 16:38:21 +0900

    ch6 boggle
    Comments resolved. Merging.

--- ch6/boggle.py
+++ ch6/boggle.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
 
+
 SIZE_OF_BOARD = 5
 MAX_NUM_OF_ALPHABET_PER_WORD = 10
 dx = [-1, -1, -1, 1, 1, 1, 0, 0]
 dy = [-1, 0, 1, -1, 0, 1, -1, 1]
+
 
 def in_range(y, x): 
     return x >= 0 and y >= 0 and x <= 4 and y <= 4
@@ -12,30 +14,24 @@ def in_range(y, x):
 def has_word(y, x, word, cur_index, cache):
     if not in_range(y, x):
         return False
-    cache[y][x][cur_index] = 1
+
+    cache[(y, x, cur_index)] = 1
+
     if board[y][x] != word[cur_index]:
         return False
-    if cur_index == len(word)-1:
+    if cur_index == len(word) - 1:
         return True
+    
     for direction in xrange(8):
-        nextY = y + dy[direction]
-        nextX = x + dx[direction]
-        if not in_range(nextY, nextX):
+        next_y = y + dy[direction]
+        next_x = x + dx[direction]
+        if not in_range(next_y, next_x):
             continue
-        if cache[nextY][nextX][cur_index+1]:
+        if cache[(next_y, next_x, cur_index + 1)]:
             continue
-        if has_word(nextY, nextX, word, cur_index+1, cache):
+        if has_word(next_y, next_x, word, cur_index + 1, cache):
             return True
     return False
-
-
-def initialize_cache():
-    cache = {} # dynamic programming - memoization
-    for y in xrange(SIZE_OF_BOARD): # initializing dp cache
-        cache[y] = defaultdict(int)
-        for x in xrange(SIZE_OF_BOARD):
-            cache[y][x] = defaultdict(int)
-    return cache
 
 
 for _ in xrange(int(raw_input())):
@@ -43,7 +39,7 @@ for _ in xrange(int(raw_input())):
     for _ in xrange(SIZE_OF_BOARD):
         board.append(raw_input().strip())
     for _ in xrange(int(raw_input())):
-        cache = initialize_cache() # cache has to be emptied for every word.
+        cache = defaultdict(int) # cache has to be emptied for every word.
         word_to_find = raw_input().strip()
         already_found = False
         for y in xrange(SIZE_OF_BOARD):
