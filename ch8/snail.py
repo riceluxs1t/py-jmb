795885b60e5078ec8f0befd349728c0a5127af8d
Seung Hun Jang <sj43@rice.edu>
2019-02-05 22:14:35 -0600

    Update snail.py

--- ch8/snail.py
+++ ch8/snail.py
@@ -5,9 +5,12 @@ sys.setrecursionlimit(10000)
 def climb(days, climbed):
     if days == m:
         return 1 if climbed >= n else 0
+
     if (days, climbed) in cache:
         return cache[(days, climbed)]
-    ret = 0.25*climb(days + 1, climbed + 1) + 0.75*climb(days + 1, climbed + 2)
+
+    ret = 0.25 * climb(days + 1, climbed + 1) + 0.75 * climb(days + 1, climbed + 2)
+
     cache[(days, climbed)] = ret
     return ret
 
